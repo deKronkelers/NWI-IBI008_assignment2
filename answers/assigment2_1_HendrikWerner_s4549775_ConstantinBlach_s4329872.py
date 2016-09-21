4fdef864c63a683bab1f07f1fbbe660c16c1e6b1
Hendrikto <hendrik.to@gmail.com>
2016-09-21 16:20:47 +0200

    Add half of 2.1.1.

--- answers/assigment2_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assigment2_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,5 +1,25 @@
 # author: Hendrik Werner s4549775
 # author Constantin Blach s4329872
 
+from pylab import *
+from scipy.stats.mstats import zscore
+import scipy.io
+
 # assignment 2.1.1
+wine_data = scipy.io.loadmat("wine.mat")
+attribute_names = wine_data["attributeNames"]
+
+columns = [wine_data["X"][:, i] for i in range(12)]
+f = figure()
+i = 1
+for values in columns:
+    s = f.add_subplot(1, 12, i)
+    s.hist(values)
+    s.set_title(attribute_names[0, i - 1][0])
+    s1 = f.add_subplot(2, 12, i)
+    s1.boxplot(zscore(values))
+    s1.set_title(attribute_names[0, i - 1][0])
+    i += 1
+show()
+
 # assignment 2.1.2
