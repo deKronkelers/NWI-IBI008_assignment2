f74e17496b6492113d39302550d579e3f0c56b7d
Hendrikto <hendrik.to@gmail.com>
2016-09-21 17:39:56 +0200

    Add other half of answer to 2.1.1.

--- answers/assigment2_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assigment2_1_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -19,4 +19,25 @@ for i, values in enumerate(columns):
     s.set_title(attribute_names[0, i][0])
 show()
 
+
+# remove outliers
+def remove_outliers(values, expected_value, factor=10):
+    return np.array(list(filter(lambda value: value < expected_value * factor, values)))
+
+
+volatile_acidity_filtered = remove_outliers(columns[1], 2)
+density_filtered = remove_outliers(columns[7], 1)
+alcohol_filtered = remove_outliers(columns[10], 5)
+
+f2 = figure()
+j = 1
+for i, values in [(1, volatile_acidity_filtered), (7, density_filtered), (10, alcohol_filtered)]:
+    s = f2.add_subplot(1, 3, j)
+    s.hist(columns[i])
+    s = f2.add_subplot(2, 3, j)
+    s.hist(values)
+    s.set_title(attribute_names[0, i][0])
+    j += 1
+show()
+
 # assignment 2.1.2
