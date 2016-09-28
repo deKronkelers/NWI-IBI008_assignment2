d9f6cea0e103814c6881f6a5155c5fff6b9df856
Hendrikto <hendrik.to@gmail.com>
2016-09-28 16:55:11 +0200

    Add an asnwer to 2.3.1 i.

--- answers/assigment2_3_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assigment2_3_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,4 +1,14 @@
 # author: Hendrik Werner s4549775
 # author Constantin Blach s4329872
 
+from numpy import array
+
 # assignment 2.3.1
+data = array([2, 3, 6, 8, 11, 18])
+
+# assignment 2.3.1 i
+print("(i)")
+m = data.mean()
+print("mean = {}".format(m))
+standard_deviation = data.std()
+print("standard deviation = {}".format(standard_deviation))
