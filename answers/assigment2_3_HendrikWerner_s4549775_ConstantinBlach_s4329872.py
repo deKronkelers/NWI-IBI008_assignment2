bd598f25edfb803f12a4dce1c053d1fc53e487c5
Hendrikto <hendrik.to@gmail.com>
2016-09-28 17:11:02 +0200

    Add asnwer to 2.3.1 ii.

--- answers/assigment2_3_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assigment2_3_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,6 +1,8 @@
 # author: Hendrik Werner s4549775
 # author Constantin Blach s4329872
 
+import itertools
+
 from numpy import array
 
 # assignment 2.3.1
@@ -12,3 +14,14 @@ m = data.mean()
 print("mean = {}".format(m))
 standard_deviation = data.std()
 print("standard deviation = {}".format(standard_deviation))
+
+# assignment 2.3.1 ii
+print("\n(ii)")
+combinations_of_2 = list(itertools.combinations(data, 2))
+print("Combinations of two aliens:\n{}".format(combinations_of_2))
+combinations_of_2_with_mean = [(array(c).mean(), c) for c in combinations_of_2]
+print("Combinations of two aliens with their mean:\n{}".format(combinations_of_2_with_mean))
+combinations_of_4 = list(itertools.combinations(data, 4))
+print("Combinations of two aliens:\n{}".format(combinations_of_4))
+combinations_of_4_with_mean = [(array(c).mean(), c) for c in combinations_of_4]
+print("Combinations of two aliens with their mean:\n{}".format(combinations_of_4_with_mean))
