204553bd4e09abda4135f84cc4ab93d075a5bd33
Hendrikto <hendrik.to@gmail.com>
2016-09-29 18:23:23 +0200

    Filter the data set and plot the first 10 images.

--- answers/assigment2_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
+++ answers/assigment2_2_HendrikWerner_s4549775_ConstantinBlach_s4329872.py
@@ -1,8 +1,11 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+# some code is taken from ex2_1_1.py
+
 from pylab import *
 from scipy.io import loadmat
 
-# Index of the digit to display
-i = 0
+# assignment 2.2.1
 
 # Load Matlab data file to python dict structure
 mat_data = loadmat('./data/zipdata.mat')
@@ -13,9 +16,26 @@ traindata = mat_data['traindata']
 X = matrix(traindata[:, 1:])
 y = matrix(traindata[:, 0])
 
-# Visualize the i'th digit as an image
-subplot(1, 1, 1)
-I = reshape(X[i, :], (16, 16))
-imshow(I, extent=(0, 16, 0, 16), cmap=cm.gray_r)
-title('Digit as an image')
+
+def filter_data(data_set, class_index, keep=None):
+    if keep is None:
+        return
+    i = 0
+    while i < class_index.shape[1]:
+        if class_index[0, i] in keep:
+            i += 1
+        else:
+            data_set = np.delete(data_set, i, 0)
+            class_index = np.delete(class_index, i)
+    return data_set, class_indexq
+
+
+X_filtered, y_filtered = filter_data(X, y, keep=[0, 1])
+
+f, grid = plt.subplots(2, 5)
+for i, x in enumerate(X_filtered[:10, :]):
+    x = reshape(x, (16, 16))
+    s = grid[i // 5, i % 5]
+    s.set_title("Image #{}".format(i))
+    s.imshow(x, extent=(0, 16, 0, 16), cmap=cm.gray_r)
 show()
